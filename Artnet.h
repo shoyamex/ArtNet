bbd938392a22d7cc0e059a0245b742fdba5f549f
shoyamex <124542315+shoyamex@users.noreply.github.com>
2023-04-29 17:20:46 +0900

    Update Artnet.h

--- Artnet.h
+++ Artnet.h
@@ -6,8 +6,8 @@
 // Packet Summary : https://art-net.org.uk/structure/packet-summary-2/
 // Packet Definition : https://art-net.org.uk/structure/streaming-packets/artdmx-packet-definition/
 
-#include "ArtnetWiFi.h"
-#include "ArtnetEther.h"
+//#include "ArtnetWiFi.h"
+//#include "ArtnetEther.h"
 
 #if !defined(ARTNET_ENABLE_WIFI) && !defined(ARTNET_ENABLE_ETHER)
 #error THIS PLATFORM HAS NO WIFI OR ETHERNET OR NOT SUPPORTED ARCHITECTURE. PLEASE LET ME KNOW!
